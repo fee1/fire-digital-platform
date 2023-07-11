3f49a9701c6d6b9134580a02de7ff2756fc147e0
zhuxiaofeng <zhuxiaofeng@flashhold.com>
2023-07-11 16:13:25 +0800

    import oauth2 test

---
+++ src/main/java/com/huajie/oauth/UserTestController.java
@@ -0,0 +1,23 @@
+package com.huajie.oauth;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用于测试使用
+ *
+ * @author zhuxiaofeng
+ * @date 2023/7/11
+ */
+@RestController
+@RequestMapping("/user")
+public class UserTestController {
+
+    @GetMapping("/getCurrentUser")
+    public Object getCurrentUser(Authentication authentication) {
+        return authentication.getPrincipal();
+    }
+
+}
