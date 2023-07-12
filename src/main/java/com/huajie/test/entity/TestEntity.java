9b0cb0b7a1f45d3b5641121019cf1c57b8538ca0
circle <15593794849@163.com>
2023-07-12 21:16:37 +0800

    init

--- src/main/java/com/huajie/test/entity/TestEntity.java
+++ src/main/java/com/huajie/test/entity/TestEntity.java
@@ -1,8 +1,10 @@
 package com.huajie.test.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("TEST_TABLE")
 public class TestEntity {
 
     private Long id;
