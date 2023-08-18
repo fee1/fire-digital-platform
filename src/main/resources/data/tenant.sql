aad0410674add219fd472a5bf9aad48549099a93
circle <15593794849@163.com>
2023-08-18 17:49:23 +0800

    基础数据上次

---
+++ src/main/resources/data/tenant.sql
@@ -0,0 +1,3 @@
+INSERT INTO fire_digital_platform.tenant
+(id, tenant_name, tenant_type, status, effective_end_date, create_time, create_user, update_time, update_user)
+VALUES(1, 'system', 'system', 1, '2073-12-01 20:33:59', NULL, '', NULL, '');
