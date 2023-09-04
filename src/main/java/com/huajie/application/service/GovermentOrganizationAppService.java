221349a8f46983f12255bb9f60e64a3287b2f969
zhuxiaofeng <zhuxiaofeng@flashhold.com>
2023-09-04 21:44:28 +0800

    重构

--- src/main/java/com/huajie/application/service/GovermentOrganizationAppService.java
+++ src/main/java/com/huajie/application/service/GovermentOrganizationAppService.java
@@ -1,27 +1,20 @@
 package com.huajie.application.service;
 
-import com.alibaba.fastjson.JSONObject;
 import com.huajie.application.api.request.AddGovermentAdminRequestVO;
 import com.huajie.application.api.request.AddGovermentOperatorRequestVO;
 import com.huajie.application.api.request.EditEnterpriseRequestVO;
 import com.huajie.application.api.request.EditGovermentInfoRequestVO;
 import com.huajie.application.api.request.UserAddRequestVO;
 import com.huajie.application.api.response.EnterpriseResponseVO;
-import com.huajie.domain.common.constants.RoleCodeConstants;
 import com.huajie.domain.common.oauth2.model.TenantModel;
 import com.huajie.domain.common.utils.UserContext;
-import com.huajie.domain.entity.Role;
 import com.huajie.domain.entity.Tenant;
 import com.huajie.domain.entity.User;
 import com.huajie.domain.service.GovermentOrganizationService;
-import com.huajie.domain.service.RoleService;
-import com.huajie.domain.service.TenantService;
-import com.huajie.domain.service.UserService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,53 +29,38 @@ public class GovermentOrganizationAppService {
     @Autowired
     private GovermentOrganizationService govermentOrganizationService;
 
-    @Autowired
-    private TenantService tenantService;
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private RoleService roleService;
-
     public void editGovermentInfo(EditGovermentInfoRequestVO requestVO) {
         Tenant tenant = new Tenant();
         BeanUtils.copyProperties(requestVO, tenant);
         if (StringUtils.isNotBlank(requestVO.getGovernmentName())){
             tenant.setTenantName(requestVO.getGovernmentName());
         }
-        if (!CollectionUtils.isEmpty(requestVO.getEntIndustryClassification())){
-            tenant.setEntIndustryClassification(JSONObject.toJSONString(requestVO.getEntIndustryClassification()));
-        }
         TenantModel currentTenant = UserContext.getCurrentTenant();
         tenant.setId(currentTenant.getId());
-        tenantService.updateById(tenant);
+        govermentOrganizationService.editGovermentInfo(tenant, requestVO.getEntIndustryClassification());
     }
 
     public void addAdminUser(AddGovermentAdminRequestVO requestVO) {
         List<UserAddRequestVO> govAdminList = requestVO.getGovAdminList();
-        Role govAdminCodeRole = roleService.getRoleByCode(RoleCodeConstants.GOV_ADMIN_CODE);
         for (UserAddRequestVO userAddRequestVO : govAdminList) {
             User user =new User();
             BeanUtils.copyProperties(userAddRequestVO, user);
-            user.setRoleId(govAdminCodeRole.getId());
-            userService.addUser(user);
+            govermentOrganizationService.addAdminUser(user);
+
         }
     }
 
     public void addOperatorUser(AddGovermentOperatorRequestVO requestVO) {
         List<UserAddRequestVO> govOperatorList = requestVO.getGovOperatorList();
-        Role govOperatorCodeRole = roleService.getRoleByCode(RoleCodeConstants.GOV_OPERATOR_CODE);
         for (UserAddRequestVO userAddRequestVO : govOperatorList) {
             User user =new User();
             BeanUtils.copyProperties(userAddRequestVO, user);
-            user.setRoleId(govOperatorCodeRole.getId());
-            userService.addUser(user);
+            govermentOrganizationService.addOperatorUser(user);
         }
     }
 
     public List<EnterpriseResponseVO> getEnterpriseVerifyList(String enterpriseName) {
-        List<Tenant> tenants = tenantService.getTenantLikeNameAndNotapprove(enterpriseName);
+        List<Tenant> tenants = govermentOrganizationService.getEnterpriseVerifyList(enterpriseName);
         List<EnterpriseResponseVO> enterpriseResponseVOList = new ArrayList<>();
         for (Tenant tenant : tenants) {
             EnterpriseResponseVO enterpriseResponseVO = new EnterpriseResponseVO();
@@ -93,21 +71,15 @@ public class GovermentOrganizationAppService {
     }
 
     public void enterpriseVerifyPass(Integer enterpriseId) {
-        Tenant tenant = new Tenant();
-        tenant.setId(enterpriseId);
-        tenant.setApproveStatus((short) 1);
-        tenantService.updateById(tenant);
+        govermentOrganizationService.enterpriseVerifyPass(enterpriseId);
     }
 
     public void editEnterprise(EditEnterpriseRequestVO requestVO) {
-        Tenant tenant = new Tenant();
-        tenant.setId(requestVO.getEnterpriseId());
-        tenant.setEntIndustryClassification(requestVO.getEntIndustryClassification());
-        this.tenantService.updateById(tenant);
+        govermentOrganizationService.editEnterprise(requestVO);
     }
 
     public List<EnterpriseResponseVO> getEnterpriseList(String enterpriseName) {
-        List<Tenant> tenants = tenantService.getTenantLikeNameAndApprove(enterpriseName);
+        List<Tenant> tenants = govermentOrganizationService.getEnterpriseList(enterpriseName);
         List<EnterpriseResponseVO> enterpriseResponseVOList = new ArrayList<>();
         for (Tenant tenant : tenants) {
             EnterpriseResponseVO enterpriseResponseVO = new EnterpriseResponseVO();
--- src/main/java/com/huajie/domain/service/GovermentOrganizationService.java
+++ src/main/java/com/huajie/domain/service/GovermentOrganizationService.java
@@ -1,6 +1,20 @@
 package com.huajie.domain.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.huajie.application.api.request.EditEnterpriseRequestVO;
+import com.huajie.domain.common.constants.RoleCodeConstants;
+import com.huajie.domain.entity.GovIndustryMap;
+import com.huajie.domain.entity.Role;
+import com.huajie.domain.entity.Tenant;
+import com.huajie.domain.entity.User;
+import com.huajie.infrastructure.mapper.GovIndustryMapMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author zhuxiaofeng
@@ -8,4 +22,80 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class GovermentOrganizationService {
+
+    @Autowired
+    private TenantService tenantService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private GovIndustryMapMapper govIndustryMapMapper;
+
+    public void editGovermentInfo(Tenant tenant, List<String> entIndustryClassification) {
+        tenantService.updateById(tenant);
+        QueryWrapper<GovIndustryMap> queryWrapper = new QueryWrapper<>();
+        queryWrapper.lambda().eq(GovIndustryMap::getTenantId, tenant.getId());
+        List<GovIndustryMap> govIndustryMaps = govIndustryMapMapper.selectList(queryWrapper);
+        //当前管理的行业
+        Set<String> currentGovIndustryMaps = govIndustryMaps.stream().map(GovIndustryMap::getIndustryClassification).collect(Collectors.toSet());
+        //新编辑管理的行业
+        Set<String> newGovIndustryMaps = new HashSet<>(entIndustryClassification);
+        // 计算出应该移除的 公式 : 1、2、3 -  3、4、5 = 1、2
+        Set<String> delGovIndustryMaps = new HashSet<>(currentGovIndustryMaps);
+        delGovIndustryMaps.removeAll(newGovIndustryMaps);
+        queryWrapper = new QueryWrapper<>();
+        queryWrapper.lambda()
+                .eq(GovIndustryMap::getTenantId, tenant.getId())
+                .in(GovIndustryMap::getIndustryClassification, delGovIndustryMaps);
+        govIndustryMapMapper.delete(queryWrapper);
+
+        // 计算出应该添加的 公式 : 3、4、5 - 1、2、3 = 4、5
+        Set<String> addGovIndustryMaps = new HashSet<>(newGovIndustryMaps);
+        addGovIndustryMaps.removeAll(currentGovIndustryMaps);
+        for (String addGovIndustryMap : addGovIndustryMaps) {
+            GovIndustryMap govIndustryMap = new GovIndustryMap();
+            govIndustryMap.setTenantId(tenant.getId());
+            govIndustryMap.setIndustryClassification(addGovIndustryMap);
+            govIndustryMapMapper.insert(govIndustryMap);
+        }
+    }
+
+    public void addAdminUser(User user) {
+        Role govAdminCodeRole = roleService.getRoleByCode(RoleCodeConstants.GOV_ADMIN_CODE);
+        user.setRoleId(govAdminCodeRole.getId());
+        userService.addUser(user);
+    }
+
+    public void addOperatorUser(User user) {
+        Role govOperatorCodeRole = roleService.getRoleByCode(RoleCodeConstants.GOV_OPERATOR_CODE);
+        user.setRoleId(govOperatorCodeRole.getId());
+        userService.addUser(user);
+    }
+
+    public List<Tenant> getEnterpriseVerifyList(String enterpriseName) {
+        return tenantService.getTenantLikeNameAndNotapprove(enterpriseName);
+    }
+
+    public void enterpriseVerifyPass(Integer enterpriseId) {
+        Tenant tenant = new Tenant();
+        tenant.setId(enterpriseId);
+        tenant.setApproveStatus((short) 1);
+        tenantService.updateById(tenant);
+    }
+
+    public void editEnterprise(EditEnterpriseRequestVO requestVO) {
+        Tenant tenant = new Tenant();
+        tenant.setId(requestVO.getEnterpriseId());
+        tenant.setEntIndustryClassification(requestVO.getEntIndustryClassification());
+        this.tenantService.updateById(tenant);
+    }
+
+
+    public List<Tenant> getEnterpriseList(String enterpriseName) {
+        return tenantService.getTenantLikeNameAndApprove(enterpriseName);
+    }
 }
