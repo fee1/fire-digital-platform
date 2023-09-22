83d1b7f54d6396937be4557ee69f95376db6e112
circle <15593794849@163.com>
2023-09-22 15:42:48 +0800

    SSL证书

---
+++ src/main/java/com/huajie/domain/common/HttpsConfig.java
@@ -0,0 +1,29 @@
+package com.huajie.domain.common;
+
+/**
+ * 同时开启http和https
+ */
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class HttpsConfig {
+    @Value("${custom.http-port:8080}")
+    private Integer httpPort;
+
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(httpPort);
+        return connector;
+    }
+}
