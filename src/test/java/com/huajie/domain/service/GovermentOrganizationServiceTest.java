35f49b3487dfc7b2d42d0ed5746ef50d27ba7af2
zhuxiaofeng <zhuxiaofeng@flashhold.com>
2023-12-26 22:39:19 +0800

    测试类获取用户信息

--- src/test/java/com/huajie/domain/service/GovermentOrganizationServiceTest.java
+++ src/test/java/com/huajie/domain/service/GovermentOrganizationServiceTest.java
@@ -6,6 +6,8 @@ import com.huajie.application.api.LoginApi;
 import com.huajie.application.api.request.LoginRequestVO;
 import com.huajie.application.api.response.LoginResponseVO;
 import com.huajie.application.service.LoginAppService;
+import com.huajie.domain.common.oauth2.model.CustomizeGrantedAuthority;
+import com.huajie.domain.common.utils.UserContext;
 import com.huajie.domain.entity.Tenant;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +18,7 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.servlet.http.HttpServletRequestWrapper;
 import java.util.ArrayList;
@@ -29,30 +32,38 @@ public class GovermentOrganizationServiceTest extends BaseTest {
     @Autowired
     private LoginAppService loginAppService;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Before
     public void userInfo(){
-        LoginRequestVO loginRequestVO = new LoginRequestVO();
-        loginRequestVO.setUsername("18039596250");
-        loginRequestVO.setPassword("123456");
-        LoginResponseVO login = loginAppService.login("admin", "admin", loginRequestVO);
+//        LoginRequestVO loginRequestVO = new LoginRequestVO();
+//        loginRequestVO.setUsername("18039596250");
+//        loginRequestVO.setPassword("123456");
+//        LoginResponseVO login = loginAppService.login("admin", "admin", loginRequestVO);
 
-
-        UserDetails userDetails = org.springframework.security.core.userdetails.User
-                .withUsername("admin")
-                .password("admin")
-                .authorities(new ArrayList<>())
-                .accountExpired(false)
-                .accountLocked(false)
-                .credentialsExpired(false)
-                .disabled(false).build();
-        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, null);
-        SecurityContextImpl securityContext = new SecurityContextImpl(usernamePasswordAuthenticationToken);
-        SecurityContextHolder.setContext(securityContext);
+        UserDetails userDetails = userDetailsService.loadUserByUsername("18811110050");
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(usernamePasswordAuthenticationToken);
     }
 
     @Test
     public void getAdminEnterpriseList() {
-        List<Tenant> adminEnterpriseList = govermentOrganizationService.getAdminEnterpriseList();
+        List<Tenant> adminEnterpriseList = govermentOrganizationService
+                .getAdminEnterpriseList(1, Integer.MAX_VALUE, "", "", 1);
+
+        List<Tenant> adminGovernmentList = govermentOrganizationService
+                .getAdminGovernmentList(1, Integer.MAX_VALUE, "");
+
+        List<Tenant> adminEnterpriseList1 = govermentOrganizationService
+                .getAdminEnterpriseList(1, Integer.MAX_VALUE, "", "", 1);
+
+        List<Tenant> adminGovernmentList2 = govermentOrganizationService
+                .getAdminGovernmentList(1, Integer.MAX_VALUE, "");
+
+
+        List<Tenant> adminEnterpriseList2 = govermentOrganizationService.getAdminEnterpriseList();
         System.out.println(adminEnterpriseList);
     }
 
