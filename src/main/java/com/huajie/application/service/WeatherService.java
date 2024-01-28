dff606a04429e2bdf6e6c56e2f51844d92ae4531
zhuxiaofeng <zhuxiaofeng@flashhold.com>
2024-01-28 22:04:11 +0800

    天气查询

--- src/main/java/com/huajie/application/service/WeatherService.java
+++ src/main/java/com/huajie/application/service/WeatherService.java
@@ -2,6 +2,7 @@ package com.huajie.application.service;
 
 import com.huajie.domain.common.exception.ServerException;
 import com.huajie.domain.common.oauth2.model.CustomizeGrantedAuthority;
+import com.huajie.domain.common.utils.GuavaUtil;
 import com.huajie.domain.common.utils.UserContext;
 import com.huajie.domain.entity.Tenant;
 import com.huajie.domain.service.UserService;
@@ -10,10 +11,9 @@ import com.huajie.infrastructure.external.weather.model.Live;
 import com.huajie.infrastructure.external.weather.model.WeatherResponseBody;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
-import java.util.List;
 import java.util.Objects;
 
 /**
@@ -27,14 +27,22 @@ public class WeatherService {
     @Autowired
     private UserService userService;
 
-    public List<Live> getWeather() {
+
+
+    public Live getWeather() {
         CustomizeGrantedAuthority userInfo = UserContext.getCustomizeGrantedAuthority();
         Tenant tenant = userInfo.getTenant();
         Integer city = tenant.getCity();
-        WeatherResponseBody weather = WeatherClient.getWeather(Integer.toString(city));
-        if (Objects.isNull(weather)){
-            throw new ServerException("天气查询异常");
+        Live live =(Live) GuavaUtil.get(String.valueOf(city));
+        if (Objects.isNull(live)) {
+            WeatherResponseBody weather = WeatherClient.getWeather(Integer.toString(city));
+
+            if (Objects.isNull(weather) && CollectionUtils.isEmpty(weather.getLives())) {
+                throw new ServerException("天气查询异常");
+            }
+            live = weather.getLives().get(0);
+            GuavaUtil.set(Integer.toString(city), live, 3600);
         }
-        return weather.getLives();
+        return live;
     }
 }
