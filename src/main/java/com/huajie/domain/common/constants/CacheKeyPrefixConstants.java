e6812eef6e72faef40417652c1a931323dd6b07a
zhuxiaofeng <zhuxiaofeng@qudian.com>
2024-02-01 18:07:30 +0800

    修改手机号 constants

--- src/main/java/com/huajie/domain/common/constants/CacheKeyPrefixConstants.java
+++ src/main/java/com/huajie/domain/common/constants/CacheKeyPrefixConstants.java
@@ -11,4 +11,9 @@ public class CacheKeyPrefixConstants {
      */
     public static final String REGISTER_VERIFICATION = "register_verification_";
 
+    /**
+     * 修改用户手机号验证缓存key前缀
+     */
+    public static final String UPDATE_USER_VERIFICATION = "update_user_verification_";
+
 }
